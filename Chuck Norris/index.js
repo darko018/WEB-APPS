1c3f56d2213f2665e28f3b432c5d914b3ae4b8d9
darko018 <darkokovacevic018@gmail.com>
2021-07-23 19:56:22 +0200

    'chuck norris app finished'

---
+++ Chuck Norris/index.js
@@ -0,0 +1,19 @@
+const buttonElement = document.querySelector('#button');
+const jokeContainer = document.querySelector('.joke-container');
+const url = 'https://api.chucknorris.io/jokes/random';
+
+
+buttonElement.addEventListener('click', () => {
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+
+        .then(data => {
+            jokeContainer.innerHTML = "";
+            let joke = data.value;
+            let $p = document.createElement("p");
+            $p.textContent = joke;
+            jokeContainer.append($p);
+        })
+});
